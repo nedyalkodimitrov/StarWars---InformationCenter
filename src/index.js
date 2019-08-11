1d2249c5157d173894c4b3fce0e795654ccf6f9a
siskomen <az23634@gmail.com>
2019-08-11 09:49:29 +0300

    design feture

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import '../src/css/index.css';
 import '../src/css/loadingCard.css';
+import '../src/css/mainCard.css';
 import {getPeople} from '../src/jedi'; 
-
 
 class App extends Component{
     constructor(props){
@@ -37,13 +37,20 @@ class StarWars extends Component{
                         <LoadingCard/>
                         <LoadingCard/>                 
                 </div>
-
             )
         }
-
+        let imageCounter = 0;
+        let imageName = "StarWars"
         return(
-            <div>
-                {this.state.people.map(person => <h1 className = 'jedi-name'>{person.name}</h1>)} 
+            <div className ="loading-container">
+                {this.state.people.map(
+                    person => {
+                           imageCounter++;
+                           return <Card  name ={person.name} image = {"/images/StarWars" + imageCounter + ".jpg"}/>
+                          
+                         }
+                    )
+                } 
             </div>  
         )
     } 
@@ -70,11 +77,27 @@ const LoadingCard = () => {
         <div className = {className + "-text-small"}></div>
         <div className = {className + "-text-big"}></div>
     </div>
+   )
+};
+
+const Card = (props) => {
+  const {name, image} = props;  
+  const className = 'card';
+
+  return (
+    <div className = {className + '-body'}>  
+        <div className = {className + "-container"} onMouseOver = {ImageShrinking()}>
+            <img className = {className + "-image"} src = {process.env.PUBLIC_URL + image} />
+            <h4 className = {className + "-text-small"}>{name}</h4>
+            <div className = {className + "-text-big"}></div>
+        </div>
+    </div>
   )
 };
 
+const ImageShrinking = () =>{
+    var image = document.getElementById("card-image");
+    image.classList.add('imageShrinking');
+}
+
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-
-
-
